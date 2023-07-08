414aae136bfc8716541432b311cd6c8fc539ce6e
404MigzNotFound <138438702+404MigzNotFound@users.noreply.github.com>
2023-07-08 07:05:29 +0000

    added requirements.txt

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [{
+  'id': 1,
+  'title': 'Data Analyst',
+  'location': 'Manila, Philippines',
+  'salary': 'P1,000,000'
+}, {
+  'id': 2,
+  'title': 'Data Scientist',
+  'location': 'QC, Philippines',
+  'salary': 'P2,000,000'
+}, {
+  'id': 3,
+  'title': 'Frontend Engineer',
+  'location': 'Remote',
+  'salary': 'P3,000,000'
+}, {
+  'id': 4,
+  'title': 'Backend Engineer',
+  'location': 'Antips, Philippines',
+}]
 
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company_name='Migz')
+
+
+@app.route("/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
